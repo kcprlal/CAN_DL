3b3f22c238f7b2b4c8bf7ad0c6cab2c0f471b5a0
Paweł Urban <urb.pa@wp.pl>
2024-10-24 22:02:01 +0200

    Testowy Upload

---
+++ Rakieta.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+int main() {
+
+    for (int i = 15; i>=1;i--){
+        system("cls");
+        cout << i <<endl;
+        Sleep(1000);
+    }
+    system("cls");
+    cout <<"JEBUT"<<endl;
+    return 0;
+}
