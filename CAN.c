13f9b14e8346aa2e0dc30d7102b924723bd08da6
Paweł Urban <urb.pa@wp.pl>
2024-10-27 17:15:19 +0100

    Update CAN.c

--- CAN.c
+++ CAN.c
@@ -1,9 +1,13 @@
 #include<stdlib.h>
 #include<avr/io.h>
+#include <util/delay.h>
+
+#define SS0 7
+#define SS1 6
 
 void spi_arduino_init()
 {
-  //SPI CHIP SELECT
+//SPI CHIP SELECT
   DDRD |=(1<<6);
   DDRD |=(1<<7);
   PORTD &=(0<<6);
@@ -30,8 +34,26 @@ void spi_arduino_init()
   SPCR |=(1<<SPE);
 //przerwania moze pozniej 
 }
+void spi_CAN_init_1()
+{
+  CAN_writereg()
 
-void can_write(){
+}
+uint8_t SPI_transfer(uint8_t data) {
+	SPDR0 = data;
+	while (!(SPSR0 & (1 << SPIF)));
+	return SPDR0;
+}
+void CAN_writereg(uint8_t reg, uint8_t value) {
+	PORTB &= ~(1 << SS0);
+	SPI_transfer(reg & ~0x80);
+ _dealy_ms(10);
+	SPI_transfer(value);
+ _dealy_ms(10);
+	PORTB |= (1 << SS0);
+}
+void can_write()
+{
   PORTD |=(1<<6);
   
 
@@ -47,7 +69,7 @@ void can_read(){
 
 int main(){
 spi_arduino_init();
-spi_CAN_init();
+spi_CAN_init_1()
 
 
   return 0;
