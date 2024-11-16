2525e0d257845c19b7216609c7149e2b4e489b31
Kacper Lalik <160536641+kcprlal@users.noreply.github.com>
2024-11-16 03:12:16 +0100

    SPI init working

--- CAN.c
+++ CAN.c
@@ -23,15 +23,15 @@ void spi_arduino_init()
 
 
 //wybor f zegara
-  SPCR &=(0<<SPR1);
+  SPCR &=~(1<<SPR1);
   SPCR |=(1<<SPR0);
 //polarity i faza zegara
-  SPCR &= (0<<CPOL);
-  SPCR &= (0<<CPHA);
+  SPCR &=~(1<<CPOL);
+  SPCR &=~(1<<CPHA);
 //master
   SPCR |=(1<<MSTR);
-//data order LSB
-  SPCR |=(1<<DORD);
+//data order MSB
+  SPCR &=~(1<<DORD);
 //spi enable
   SPCR |=(1<<SPE);
 //przerwania moze pozniej 
@@ -45,24 +45,27 @@ uint8_t SPI_transfer(uint8_t data) {
 //spraedzic czy slave wybierany jest za pomoca 0 czy 1?
 
 void can_write(uint8_t reg, uint8_t value){
-  PORTD &=~(1<<PB2);
-  SPI_transfer(reg & ~0x80);
- _delay_ms(10);
+  PORTB &=~(1<<PB2);
+  SPI_transfer(0x02);
+  SPI_transfer(reg);
 	SPI_transfer(value);
- _delay_ms(10);
-  PORTD |=(1<<PB2);
+  PORTB |=(1<<PB2);
 }
 
 void can_read(uint8_t reg, void *reg_data, uint8_t len){
-  PORTD &=~(1<<PB2);
-  SPI_transfer(reg | 0x80);
+  
+  PORTB &=~(1<<PB2);
+  SPI_transfer(0x03);
+  SPI_transfer(reg);
+
 		while(len--)
 		{
-			*(uint8_t*)reg_data=SPI_transfer(0x00);
-			(uint8_t*)reg_data++;
+			 *(uint8_t*)reg_data = SPI_transfer(0x00); // Cast to uint8_t*
+        reg_data = (uint8_t*)reg_data + 1;        // Increment as uint8_t*
+        _delay_ms(10);
 		}
 
-  PORTD |=(1<<PB2);
+  PORTB |=(1<<PB2);
 }
 
 void can_standard(){
@@ -88,29 +91,31 @@ void can_standard(){
 
 void can_init(){
   PORTB &= ~(1 << PB2);
-  can_write(CANCTRL, 0b10000000); //zrob 16
+  can_write(CANCTRL, 0x80);
   can_write(CNF1, 0x41); 
   can_write(CNF2, 0xFB);
   can_write(CNF3, 0x86);
   can_standard();
   can_write(RXB0CNTRL, 0x64);
   can_write(CANCTRL, 0x00);
+  PORTB |=(1<<PB2);
 }
 
 int main(){
-  DDRB |=(1<<PB5);
-  Serial.begin(31250);
-uint8_t read_data;
+
+DDRD |=(1<<PD2);
+PORTD &= ~(1 << PD2);
+
+uint8_t canctrl;
 spi_arduino_init();
 can_init();
-int arr[8];
-can_read(CNF2, arr, 8);
+can_read(CANCTRL, &canctrl, 1);
 _delay_ms(100);
-for(int i = 0; i<8; ++i){
-  if(arr[i]==1) PORTB |=(1<<PB5);
-  _delay_ms(1000);
-  PORTB &=~(1<<PB5);
-}
+ if ((canctrl & 0xE0) == 0x00) { // 000 dla trybu normalnego
+        PORTD |= (1 << PD2);        // Ustawienie PD2 na wysoki
+    } else {
+        PORTD &= ~(1 << PD2);       // Ustawienie PD2 na niski
+    }
 
   return 0;
 }
