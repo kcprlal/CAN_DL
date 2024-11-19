3447fd8b6a2867b7061d7a0ea6680113e3467bcf
Paweł Urban <urb.pa@wp.pl>
2024-11-19 22:47:36 +0100

    samll corrections
    unsure about spi init

--- leonardodavinci.ino
+++ leonardodavinci.ino
@@ -14,7 +14,10 @@ void spi_arduino_init()
   DDRB |=(1<<PB6);
   //tutaj zmiana z jakeigos powodu trzrba skontrolowac
   PORTB |=(1<<PB6);
+
 //Zakladamy ze reszta interfaceu spi jest zkonfigurowana
+//jeśli nie to trzeba sprawdzić jak się dostać do icsp
+
 //wybor f zegara
   SPCR &=(0<<SPR1);
   SPCR |=(1<<SPR0);
@@ -24,7 +27,7 @@ void spi_arduino_init()
 //master
   SPCR |=(1<<MSTR);
 //data order LSB
-  SPCR |=(1<<DORD);
+  SPCR &=(0<<DORD);
 //spi enable
   SPCR |=(1<<SPE);
 //przerwania moze pozniej 
@@ -35,25 +38,28 @@ uint8_t SPI_transfer(uint8_t data) {
 	while (!(SPSR & (1 << SPIF)));
 	return SPDR;
 }
-//spraedzic czy slave wybierany jest za pomoca 0 czy 1?
+//sprawdzic czy slave wybierany jest za pomoca 0 czy 1?
 
 void can_write(uint8_t reg, uint8_t value){
   PORTB &=~(1<<PB6);
-  SPI_transfer(reg & ~0x80);
- _delay_ms(10);
+  SPI_transfer(0x02);
+  //możliwe że potrzebny będzie delay 10ms
+  SPI_transfer(reg);
 	SPI_transfer(value);
- _delay_ms(10);
   PORTB |=(1<<PB6);
 }
 
 void can_read(uint8_t reg, void *reg_data, uint8_t len){
   PORTB &=~(1<<PB6);
-  SPI_transfer(reg | 0x80);
+  SPI_transfer(0x03);
+  SPI_transfer(reg);
+
 		while(len--)
 		{
-			*(uint8_t*)reg_data=SPI_transfer(0x00);
-			(uint8_t*)reg_data++;
-		}
+			 *(uint8_t*)reg_data = SPI_transfer(0x00); // Cast to uint8_t*
+        reg_data = (uint8_t*)reg_data + 1;        // Increment as uint8_t*
+        _delay_ms(10);
+    }
 
   PORTB |=(1<<PB6);
 }
@@ -78,7 +84,7 @@ void can_standard(){
   }
 }
 void can_init(){
-  PORTB &= ~(1 << PB2);
+  PORTB &= ~(1 << PB6);
   can_write(CANCTRL, 0b10000000); //zrob 16
   can_write(CNF1, 0x41); 
   can_write(CNF2, 0xFB);
@@ -86,9 +92,9 @@ void can_init(){
   can_standard();
   can_write(RXB0CNTRL, 0x64);
   can_write(CANCTRL, 0x00);
+  PORTB |=(1<<PB6);
 }
 int main(){
-uint8_t read_data;
 spi_arduino_init();
 can_init();
 
